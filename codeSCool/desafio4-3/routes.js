08c805892b54b179ab6f02746b9d380823fc37b4
MorenaNobre <morenagnobre@gmail.com>
2020-07-25 19:57:05 -0300

    teachersForm

--- codeSCool/desafio4-3/routes.js
+++ codeSCool/desafio4-3/routes.js
@@ -9,6 +9,14 @@ routes.get('/teachers', function(req, res) {
     return res.render('teachers/index')
 })
 
+routes.get('/teachers/create', function(req, res) {
+    return res.render('teachers/create')
+})
+
+routes.post('/teachers', function(req, res) {
+    return res.send('recebido')
+})
+
 routes.get('/students', function(req, res) {
     return res.send('students')
 })
