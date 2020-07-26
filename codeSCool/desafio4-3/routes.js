6a19e19285b3871f03447781cd7efba241a2c5a0
MorenaNobre <morenagnobre@gmail.com>
2020-07-26 18:33:17 -0300

    challenge4-3done

--- codeSCool/desafio4-3/routes.js
+++ codeSCool/desafio4-3/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const teachers = require('./teachers')
 
 routes.get('/', function(req, res) {
     return res.redirect('/teachers')
@@ -13,9 +14,7 @@ routes.get('/teachers/create', function(req, res) {
     return res.render('teachers/create')
 })
 
-routes.post('/teachers', function(req, res) {
-    return res.send('recebido')
-})
+routes.post('/teachers', teachers.post)
 
 routes.get('/students', function(req, res) {
     return res.send('students')
