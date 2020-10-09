6cc561f66b49db939f3d92ca3997cd5d1a3a51c1
MorenaNobre <morenagnobre@gmail.com>
2020-10-09 14:02:37 -0300

    added-admin-files

--- Foodfy/foodfy-admin/routes.js
+++ Foodfy/foodfy-admin/routes.js
@@ -18,11 +18,29 @@ routes.get('/recipe-detail', function(req, res) {
   return res.render('recipe-detail')
 })
 
+routes.get('/admin/recipes/create', function(req, res) {
+  return res.render('admin/recipes/create')
+})
+
+routes.get('/admin/recipes/fields_create', function(req, res) {
+  return res.render('admin/recipes/fields_create')
+})
+
 routes.use(function(req, res) {
   res.status(404).render('not-found')
 })
 
+
 //admin routes
+
+// routes.get('/admin/recipes', recipes.index)
+// routes.get('/admin/recipes/create', recipes.create)
+// routes.get('/admin/recipes/:id', recipes.show)
+// routes.get('/admin/recipes/:id/edit', recipes.edit)
+
+// routes.post('/admin/recipes', recipes.post)
+// routes.put('/admin/recipes', recipes.put)
+// routes.delete('/admin/recipes', recipes.delete)
 
 
 
